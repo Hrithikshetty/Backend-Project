c98f4e7fccd0480de8de208418b4d86a3d8929a1
Hrithikshetty <hrithik.cd21@sahyadri.edu.in>
2023-12-22 00:13:49 +0530

    backend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import dotenv from 'dotenv';
-import connectDB from "./db/db.js"
-
+import connectDB from "./db/db.js";
+import app from './app.js';
 
 dotenv.config({
   path: './env',
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server started on port ${process.env.PORT || 8000}`);
+    });
+  })
+
+  .catch((err) => {
+    console.log("MongoDB connection failed !!", err);
+  });
